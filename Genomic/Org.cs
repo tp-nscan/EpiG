9f5d128634dd2cf045bd6d9dab459dfef1aab79f
Tom Patterson <jemboo@gmail.com>
2014-05-13 16:41:56 -0400

    added new projects

---
+++ Genomic/Org.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Genomic.Genomes;
+using MathUtils.Collections;
+
+namespace Genomic
+{
+    public interface IOrg<G> : IGuid where G : IGenome
+    {
+        IOrgBuilder<G> Builder { get; }
+        G Genome { get; }
+    }
+
+    public class Org<G> : IOrg<G> where G : IGenome
+    {
+        public Guid Guid { get; private set; }
+        public IOrgBuilder<G> Builder { get; private set; }
+        public G Genome { get; private set; }
+    }
+
+    public interface IOrgBuilder<G> where G : IGenome
+    {
+        IOrg<G> Make();
+    }
+
+    public interface IPhenotypeBuilder<G, P> where G : IGenome
+    {
+        P Make(G genome);
+    }
+}
