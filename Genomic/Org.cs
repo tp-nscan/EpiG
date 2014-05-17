df9a2c0a5427ff268d3a61476d7c31561b2141cc
Tom Patterson <jemboo@gmail.com>
2014-05-17 00:48:27 -0400

    population class

--- Genomic/Org.cs
+++ Genomic/Org.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Genomic.Genomes;
 using MathUtils.Collections;
 
@@ -26,4 +28,59 @@ namespace Genomic
     {
         P Make(G genome);
     }
+
+    public interface IPopulationBuilder<G> where G : IGenome
+    {
+        IEnumerable<IOrg<G>> MakeOrgs();
+    }
+
+    public interface IPopulation<G> : IGuid where G : IGenome
+    {
+        IEnumerable<IOrg<G>> Orgs { get; }
+        IPopulationBuilder<G> Bulder { get; }
+    }
+
+    public static class Population
+    {
+         public static PopulationImpl<G> Make<G>(this IPopulationBuilder<G> bulder, Guid guid)
+                    where G : IGenome
+         {
+             var orgs = bulder.MakeOrgs();
+
+             return new PopulationImpl<G>(
+                    guid: guid,
+                    orgs: orgs,
+                    bulder: bulder
+                 );
+         }
+    }
+
+    public class PopulationImpl<G> : IPopulation<G> where G : IGenome
+    {
+        public PopulationImpl(Guid guid, IEnumerable<IOrg<G>> orgs, IPopulationBuilder<G> bulder)
+        {
+            _guid = guid;
+            _orgs = orgs.ToList();
+            _bulder = bulder;
+        }
+
+        private readonly Guid _guid;
+        public Guid Guid
+        {
+            get { return _guid; }
+        }
+
+        private readonly IReadOnlyList<IOrg<G>> _orgs;
+        public IEnumerable<IOrg<G>> Orgs
+        {
+            get { return _orgs; }
+        }
+
+        private readonly IPopulationBuilder<G> _bulder;
+        public IPopulationBuilder<G> Bulder
+        {
+            get { return _bulder; }
+        }
+    }
+
 }
