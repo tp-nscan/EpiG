e88c810b313fe1a2bd1926af192695e390df17b7
Tom Patterson <jemboo@gmail.com>
2014-06-19 14:46:49 -0400

    removed GenomeOld

--- Genomic/Genomes/Builders/GenomeBuilder.cs
+++ Genomic/Genomes/Builders/GenomeBuilder.cs
@@ -33,7 +33,72 @@ namespace Genomic.Genomes.Builders
 
     public static class GenomeBuilder
     {
+        //        public static IEnumerable<IGenomeBuilderOld<IGenome>> MakeGenerators
+        //            (
+        //                int seed,
+        //                int builderCount,
+        //                uint symbolCount,
+        //                int sequenceLength
+        //            )
+        //        {
 
+        //            var randy = Rando.Fast(seed);
+
+        //            return Enumerable.Range(0, builderCount)
+        //                             .Select
+        //                             (
+        //                                 i=> MakeGenerator
+        //                                     (
+        //                                        symbolCount: symbolCount,
+        //                                        sequenceLength: sequenceLength,
+        //                                        seed: randy.NextInt(), 
+        //                                        guid: randy.NextGuid()
+        //                                    )
+        //                             );
+        //        }
+
+
+        //public static IGenomeBuilder MakeGenerator
+        //    (
+        //        uint symbolCount,
+        //        int sequenceLength,
+        //        int seed,
+        //        Guid guid
+        //    )
+        //{
+        //    return new GenomeBuilderRandom
+        //        (
+        //            symbolCount: symbolCount,
+        //            sequenceLength: sequenceLength,
+        //            seed: seed,
+        //            guid: guid
+        //        );
+        //}
+
+        //        public static ISingleGenomeMutator<IGenome> MakeMutator
+        //            (
+        //                Guid guid,
+        //                IGenome sourceGenomeOld,
+        //                uint symbolCount,
+        //                int seed,
+        //                double deletionRate,
+        //                double insertionRate,
+        //                double mutationRate
+        //            )
+        //        {
+        //            {
+        //                return new GenomeBuilderMutator
+        //                    (
+        //                        symbolCount: symbolCount,
+        //                        sourceGenomeOld: sourceGenomeOld,
+        //                        seed: seed,
+        //                        deletionRate: deletionRate,
+        //                        insertionRate: insertionRate,
+        //                        mutationRate: mutationRate,
+        //                        guid: guid
+        //                    );
+        //            }
+        //        }
     }
 
 
@@ -96,4 +161,86 @@ namespace Genomic.Genomes.Builders
 
 
     }
+
+    //    public class GenomeBuilderMutator : ISingleGenomeMutator<IGenome>
+    //    {
+    //        public GenomeBuilderMutator
+    //            (
+    //                Guid guid,
+    //                IGenome sourceGenomeOld,
+    //                uint symbolCount, 
+    //                int seed, 
+    //                double deletionRate, 
+    //                double insertionRate, 
+    //                double mutationRate
+    //            )
+    //        {
+    //            _sourceGenomeOld = sourceGenomeOld;
+    //            _seed = seed;
+    //            _deletionRate = deletionRate;
+    //            _insertionRate = insertionRate;
+    //            _mutationRate = mutationRate;
+    //            _guid = guid;
+    //            _symbolCount = symbolCount;
+    //        }
+
+    //        private readonly IGenome _sourceGenomeOld;
+    //        public IGenome SourceGenomeOld
+    //        {
+    //            get { return _sourceGenomeOld; }
+    //        }
+
+    //        private readonly uint _symbolCount;
+
+    //        public uint SymbolCount
+    //        {
+    //            get { return _symbolCount; }
+    //        }
+
+    //        public IGenome Make()
+    //        {
+    //            return Genome.Make
+    //                (
+    //                    sequence: SourceGenomeOld.Sequence,
+    //                    genomeBuilder: this
+    //                );
+    //        }
+
+    //        public GenomeBuilderType GenomeBuilderType
+    //        {
+    //            get { return GenomeBuilderType.SimpleSingleParent; }
+    //        }
+
+    //        private readonly int _seed;
+    //        public int Seed
+    //        {
+    //            get { return _seed; }
+    //        }
+
+    //        private readonly double _deletionRate;
+    //        public double DeletionRate
+    //        {
+    //            get { return _deletionRate; }
+    //        }
+
+    //        private readonly double _insertionRate;
+    //        public double InsertionRate
+    //        {
+    //            get { return _insertionRate; }
+    //        }
+
+    //        private readonly double _mutationRate;
+    //        public double MutationRate
+    //        {
+    //            get { return _mutationRate; }
+    //        }
+
+    //        private readonly Guid _guid;
+    //        private IGenome _sourceGenome;
+
+    //        public Guid Guid
+    //        {
+    //            get { return _guid; }
+    //        }
+    //    }
 }
